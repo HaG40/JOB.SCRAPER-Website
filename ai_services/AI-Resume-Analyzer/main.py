fb466f587a36c14562dc8a54ef7ed9294568376b
HaG40 <jorjay2546@gmail.com>
2026-05-21 00:15:10 +0700

    CORS Edited

--- ai_services/AI-Resume-Analyzer/main.py
+++ ai_services/AI-Resume-Analyzer/main.py
@@ -30,8 +30,7 @@ app = FastAPI(title="AI Resume Analyzer API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://job-scraper-frontend-fawn.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
