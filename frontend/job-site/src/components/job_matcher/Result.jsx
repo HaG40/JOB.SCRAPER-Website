5094bbee36482e24dbe5ca0ec8ed5fd277255f74
HaG40 <jorjay2546@gmail.com>
2026-05-21 18:38:30 +0700

    Fix Refetching

--- frontend/job-site/src/components/job_matcher/Result.jsx
+++ frontend/job-site/src/components/job_matcher/Result.jsx
@@ -171,7 +171,7 @@ export default function Result() {
     }
   }, [isReady]);
 
-  if (result && Object.values(result).length > 0) {
+  if (result) {
     return 
   } else if (isReady && hasCV) {
     useEffect(() => {
